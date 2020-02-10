e295b7e213bf4050448e284af2fda7fc427019ca
utakgames <60888280+utakgames@users.noreply.github.com>
2020-02-10 18:25:36 +0300

    Add files via upload

---
+++ kamerakontrol.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kamerakontrol : MonoBehaviour {
+
+    public GameObject top;
+    Vector3 mesafe;
+	void Start ()
+    {
+        mesafe = transform.position - top.transform.position;
+	}
+	
+	
+	void LateUpdate ()
+    {
+        transform.position = top.transform.position + mesafe;
+	}
+}
